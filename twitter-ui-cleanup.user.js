2132b23b3af4ad49ed8995bc5a4a6d45d07c8177
Daminator4113 <43492665+Daminator4113@users.noreply.github.com>
2025-01-18 13:45:29 +0100

    Update twitter-ui-cleanup.user.js
    Added a better way to remove parent + remove grok drawer and grok profil resume

--- twitter-ui-cleanup.user.js
+++ twitter-ui-cleanup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter UI Cleanup
 // @namespace    https://github.com/Daminator4113/Twitter-UI-Cleanup
-// @version      1.0
+// @version      1.1
 // @author       Daminator4113
 // @description  Remove unwanted buttons and sections on Twitter
 // @license      MIT
@@ -13,27 +13,42 @@
 (function() {
     'use strict';
 
+    const removeParent = (selector, levels, tagName) => {
+        const element = document.querySelector(selector);
+        if (element) {
+            let parent = element;
+            for (let i = 0; i < levels; i++) {
+                parent = parent.parentElement;
+                if (!parent) break;
+            }
+            if (parent?.tagName === tagName) {
+                //console.log('REMOVE',parent);
+                parent.remove();
+            }
+        }
+    };
+
+    // The order of removal is important!
     const removeElements = () => {
+        removeParent('aside a[href*="/i/premium_sign_up"]', 3, 'DIV'); // "Try Premium for free" section
+
         const selectors = [
             'a[href*="/i/grok"]', // Grok
             'a[href*="/i/premium_sign_up"]', // Twitter Blue
             'a[href*="/i/verified-orgs-signup"]', // Verified organizations
-            'button[data-testid="grokImgGen"]' // "Generate Image with Grok" button
+            'button[data-testid="grokImgGen"]', // "Generate Image with Grok" button.
+            'div[data-testid="GrokDrawer"]', // Grok Drawer
         ];
-
-        const premiumLinkContainer = document.querySelector('aside a[href*="/i/premium_sign_up"]')?.closest('div')?.parentElement; // "Try Premium for free" section
-        if (premiumLinkContainer?.tagName === 'DIV') {
-            premiumLinkContainer.remove();
-        }
 
         selectors.forEach(selector => {
             document.querySelectorAll(selector).forEach(el => el.remove());
         });
+
+        const grokSVG = 'path[d="M2.205 7.423L11.745 21h4.241L6.446 7.423H2.204zm4.237 7.541L2.2 21h4.243l2.12-3.017-2.121-3.02zM16.957 0L9.624 10.435l2.122 3.02L21.2 0h-4.243zm.767 6.456V21H21.2V1.51l-3.476 4.946z"]'
+        removeParent(grokSVG, 4, 'BUTTON'); // Grok profil resume
     };
 
-    const observer = new MutationObserver(() => {
-        removeElements();
-    });
+    const observer = new MutationObserver(removeElements);
 
     observer.observe(document.body, {
         childList: true,
